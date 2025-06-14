00fff53673e4d4ae91316bb9aa534799db0d41f9
heikkuj <heikkuamo@gmail.com>
2025-06-14 15:17:45 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Header (home-button)</h1>
+      </div>
+
+      <div>
+        <b>navbar</b>
+        <ul>
+          <li>Menu</li>
+          <li>Online Storefront</li>
+          <li>Locations</li>
+          <li>About us</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div>
+        <p>Hero-image</p>
+        <p>Call-to-action button</p>
+      </div>      
+
+    </div>
+  );
+}
