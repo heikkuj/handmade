4af7a3d7246616cfea05aa6980f60881c1a11554
heikkuj <heikkuamo@gmail.com>
2025-06-14 17:03:02 +0200

    added nav menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,13 @@
+'use client'
+
 import Image from "next/image";
+import Link from "next/link";
+import Navigation from '@/components/Navigation';
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <h1>Header (home-button)</h1>
-      </div>
-
-      <div>
-        <b>navbar</b>
-        <ul>
-          <li>Menu</li>
-          <li>Online Storefront</li>
-          <li>Locations</li>
-          <li>About us</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      <div>
-        <p>Hero-image</p>
-        <p>Call-to-action button</p>
-      </div>      
-
+      <Navigation />
     </div>
   );
 }
