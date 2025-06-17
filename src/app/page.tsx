1c3a0e4ba137aaf3d01f668119ff4881db409fea
heikkuj <heikkuamo@gmail.com>
2025-06-17 16:47:03 +0200

    added circular text "header"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 
-import Image from "next/image";
-import Link from "next/link";
 import Navigation from '@/components/Navigation';
 import Footer from "@/components/Footer";
+import CircularText from '@/components/CircularText';
 
 export default function Home() {
   return (
     <div>
+      <div className='p-3'>
+        <CircularText
+        text="HANDMADE*BAKERIES*"
+        onHover='slowDown'
+        spinDuration={20}
+        className='custom-class' />
+      </div>
       <Navigation />
       <Footer />
     </div>
