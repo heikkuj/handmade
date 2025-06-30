e09ea466c6c44a68b508c551ec7cec1656997e2e
heikkuj <heikkuamo@gmail.com>
2025-06-30 14:50:06 +0200

    resized images to the same ratio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import CircularText from '@/components/CircularText';
 import HeroImg from '@/components/HeroImg';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -20,23 +21,30 @@ export default function Home() {
       <HeroImg />
 
       <div>
-        <Card className='mx-3 my-8 text-center bg-gray-50'>
+        <Card className='
+        mx-5 sm:mx-10 md:mx-15 
+        my-8 sm:my-10 
+        text-sm sm:text-2xl md:text-2xl lg:text-2xl
+        text-center  bg-gray-50 md:bg-blue-600 lg:bg-fuchsia-300'>
           <CardHeader>
             <CardTitle>Our specialities</CardTitle>
           </CardHeader>
-          <CardContent className='grid grid-cols-2 gap-3'>
+          <CardContent className='grid grid-cols-2 gap-3 sm:gap-6'>
 
             <Card>
               <CardHeader>
                 <CardTitle>Macrons</CardTitle>
               </CardHeader>
               <CardContent>
-                <img 
-                src={'/pastel/macron-mix.jpg'} 
-                alt='Macrons'
-                width={'fill'}
-                height={'30%'}
-                className='rounded-md' />
+                <div className='relative place-self-center max-w-[120%] max-h-[100%]
+                w-30 h-30 
+                sm:w-56 sm:h-56'>
+                  <Image 
+                  src={'/pastel/macron-mix.jpg'} 
+                  fill
+                  alt='Macrons'
+                  className='rounded-md object-cover' />
+                </div>
               </CardContent>
             </Card>
 
@@ -45,10 +53,15 @@ export default function Home() {
                 <CardTitle>Donuts</CardTitle>
               </CardHeader>
               <CardContent>
-                <img 
-                src={'/pastel/donut.jpg'} 
-                alt='Donuts' 
-                className='rounded-md'/>
+                <div className='relative place-self-center max-w-[120%] max-h-[100%]
+                w-30 h-30 
+                sm:w-56 sm:h-56'>
+                  <Image 
+                  src={'/pastel/donut.jpg'} 
+                  fill
+                  alt='Donuts' 
+                  className='rounded-md object-cover'/>
+                </div>
               </CardContent>
             </Card>
 
@@ -57,10 +70,15 @@ export default function Home() {
                 <CardTitle>Cakes</CardTitle>
               </CardHeader>
               <CardContent>
-                <img 
-                src={'/pastel/cake.jpg'} 
-                alt='Cakes' 
-                className='rounded-md'/>
+                <div className='relative place-self-center max-w-[120%] max-h-[100%]
+                w-30 h-30 
+                sm:w-56 sm:h-56'>
+                  <Image 
+                  src={'/pastel/cake.jpg'}
+                  fill 
+                  alt='Cakes' 
+                  className='rounded-md'/>
+                </div>
               </CardContent>
             </Card>
 
@@ -69,10 +87,15 @@ export default function Home() {
                 <CardTitle>Drinks</CardTitle>
               </CardHeader>
               <CardContent>
-                <img 
-                src={'/pastel/drink.jpg'} 
-                alt='Drinks'
-                className='rounded-md' />
+                <div className='relative place-self-center max-w-[120%] max-h-[100%]
+                w-30 h-30 
+                sm:w-56 sm:h-56'>
+                  <Image 
+                  src={'/pastel/drink.jpg'} 
+                  fill
+                  alt='Drinks'
+                  className='rounded-md' />
+                </div>
               </CardContent>
             </Card>
 
